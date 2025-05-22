68b34cef60b8bd34f71c07216b1c4404ebb80b59
octaviav <octavia7104@gmail.com>
2025-05-23 00:38:53 +0200

    Basic application

---
+++ 0x01-react_intro/task-0/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import logo from './alx-logo.jpg';
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={logo} alt="ALX logo" />
+        <h1>School dashboard</h1>
+      </div>
+      
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
