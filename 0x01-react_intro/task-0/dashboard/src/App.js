72942d55a7ca151257e1f4f274964ad6f67818f4
octaviav <octavia7104@gmail.com>
2025-06-16 19:13:28 +0200

    task 0 - Basic application

--- 0x01-react_intro/task-0/dashboard/src/App.js
+++ 0x01-react_intro/task-0/dashboard/src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
-import logo from './alx-logo.jpg';
+import logo from './logo.jpg';
 
 function App() {
-  return (
-    <div>
-      <div className="App-header">
-        <img src={logo} alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
-      
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-      </div>
-      
-      <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img className="logo" src={logo} alt="logo"></img>
+                <h1>School dashboard</h1>
+            </header>
+            <div className="App-body">
+                <p>
+                    Login to access the full dashboard
+                </p>
+            </div>
+            <div className="App-footer">
+                <p>
+                    Copyright 2020 - holberton School
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
